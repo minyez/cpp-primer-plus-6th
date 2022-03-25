568c557ec24ccc5c386f75add9997c450adfd043
minyez <stevezhang@pku.edu.cn>
2022-03-25 18:14:39 +0800

    before 14.3

---
+++ ch14/play_va.cpp
@@ -0,0 +1,13 @@
+/*
+ * Date  : 2022-03-25 16:39:26
+ * Author: Min-Ye Zhang
+ * Usage : 
+ * TODO  : 
+ */
+#include <valarray>
+
+int main(int argc, char** argv)
+{
+    std::valarray<double> va1;
+    return 0;
+}
